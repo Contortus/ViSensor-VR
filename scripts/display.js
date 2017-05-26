4e0a4ce4c38aead6fe9b38198c5c09a08dfea054
Contortus <jean-joseph.buesche@uni-konstanz.de>
2017-05-26 18:20:55 +0200

    implemented basic movement

--- scripts/display.js
+++ scripts/display.js
@@ -1,3 +1,42 @@
+var haveEvents = 'ongamepadconnected' in window;
+var controllers = {};
+
+var CONTROLLER_THRESHOLD = 0.3;
+var ROTATION_SPEED = 2;
+var MOVEMENT_SPEED = 0.2;
+
+function connecthandler(e) {
+	controllers[gamepad.index] = gamepad;
+
+	requestAnimationFrame(updateStatus);
+}
+
+function disconnecthandler(e) {
+	delete controllers[gamepad.index];
+}
+
+function scangamepads() {
+	var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+	for (var i = 0; i < gamepads.length; i++) {
+		if (gamepads[i]) {
+			if (gamepads[i].index in controllers) {
+				controllers[gamepads[i].index] = gamepads[i];
+			} else {
+				controllers[gamepad.index] = gamepad;
+				requestAnimationFrame(updateStatus);
+			}
+		}
+	}
+}
+
+
+window.addEventListener("gamepadconnected", connecthandler);
+window.addEventListener("gamepaddisconnected", disconnecthandler);
+
+if (!haveEvents) {
+	setInterval(scangamepads, 500);
+}
+
 $(document).ready(function () {
 	var sensor = window.location.search.substr(1).split("=")[1];
 	var sensorData = [];
@@ -67,11 +106,15 @@ function display(data) {
 	function render() {
 		requestAnimationFrame(render);
 
+		if (!haveEvents)
+			scangamepads();
+
 		var cameraPos = {
 			"x": 0,
 			"y": 0,
 			"z": 0
 		};
+		var j;
 
 		var cameraRotation = cameraPos;
 
@@ -81,12 +124,37 @@ function display(data) {
 		if (d3.select('a-camera').attr("rotation") != null)
 			cameraRotation = d3.select('a-camera').attr("rotation");
 
-		console.log(cameraRotation);
+		for (j in controllers) {
 
-		d3.select('a-camera').attr('rotation', function () {
-			cameraRotation['y'];
-			return cameraRotation;
-		});
+			var controller = controllers[j];
+
+			var axes = controller["axes"];
+
+			var lr_axis = axes[0];
+			var fb_axis = axes[1];
+			var rot_axis = axes[2];
+
+
+			d3.select('a-camera').attr('rotation', function () {
+				if (Math.abs(rot_axis) >= CONTROLLER_THRESHOLD)
+					cameraRotation['y'] -= rot_axis * ROTATION_SPEED;
+				return cameraRotation;
+			});
+
+			d3.select('a-camera').attr('position', function () {
+				if (Math.abs(lr_axis) >= CONTROLLER_THRESHOLD)
+					cameraPos['x'] += lr_axis * MOVEMENT_SPEED;
+				return cameraPos;
+			});
+
+			d3.select('a-camera').attr('position', function () {
+				if (Math.abs(fb_axis) >= CONTROLLER_THRESHOLD)
+					cameraPos['z'] += fb_axis * MOVEMENT_SPEED;
+				return cameraPos;
+			});
+
+
+		}
 	}
 	render();
 }
